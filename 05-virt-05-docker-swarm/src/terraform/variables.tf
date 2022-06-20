ca82e30b9ea1d2e1d63d33e512d8d7119a272aaa
Олег Низамов <35757313+olegnizamov@users.noreply.github.com>
2022-06-20 19:52:29 +0400

    Домашнее задание "Оркестрация кластером Docker контейнеров на примере Docker Swarm."

--- 05-virt-05-docker-swarm/src/terraform/variables.tf
+++ 05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -1,17 +1,17 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1gtitubqcoaoesmi8vd"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1gvcj5c7qi25j81c8ob"
 }
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd884f65mo7hpqd0suh4"
+  default = "fd85grkehfr9dk3m07ot"
 }
